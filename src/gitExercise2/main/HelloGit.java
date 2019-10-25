dcf73636c7918fab49f6f3a3d46dbd155e4cdf0e
Paolo Condello <56863644+Condi7@users.noreply.github.com>
2019-10-25 12:19:45 +0200

    Sono il secondo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -4,6 +4,6 @@ public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
 		System.out.println("Questa e' una nuovissima stampa!");
-		System.out.println("Questa stringa e' nuova!");
+		System.out.println("Questa stringa e' stranuova!");
 	}
 }
