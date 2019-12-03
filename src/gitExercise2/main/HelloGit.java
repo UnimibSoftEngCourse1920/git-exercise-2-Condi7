c711b936c93cafff96600d69daf8429c295a7252
Paolo Condello <p.condello@campus.unimib.it>
2019-12-03 10:10:00 +0100

    Sono il primo developer e ho risolto un coflitto.

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
 		System.out.println("Questa e' una nuovissima stampa!");
-        System.out.println("Questa stringa e' palesemente nuova!")
+        System.out.println("Questa stringa e' palesemente nuovissima!")
 		System.out.println("Questa stringa e' stranuova!");
 	}
 }
